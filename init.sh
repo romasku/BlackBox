1f965e131e2fbe79de42e9c21aeff8cabf501bf7
frol <frolvlad@gmail.com>
2015-07-18 15:43:25 +0300

    Updated init.sh so it cache and reuse plugins across docker-rebuilds

--- init.sh
+++ init.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+
+# bash flags: (-e) Stop executing on first error and (-x) show executed commands
+set -e -x
+
 ionic platforms add android
+
 cordova plugin add cordova-plugin-splashscreen
 cordova plugin add com.ionic.keyboard
-cordova plugin add https://github.com/cogitor/PhoneGap-OrientationLock.git
-cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-globalization.git
+
+# This workaround is for Docker environment
+function cordova_plugin_add_git() {
+    repo_url="$1"
+    cached_plugin_path="/tmp/$(basename "$repo_url")"
+    if [ ! -e "$cached_plugin_path" ]
+    then
+        git clone --depth=1 "$repo_url" "$cached_plugin_path"
+    fi
+    cordova plugin add "$cached_plugin_path"
+}
+
+cordova_plugin_add_git "https://github.com/cogitor/PhoneGap-OrientationLock.git"
+cordova_plugin_add_git "https://git-wip-us.apache.org/repos/asf/cordova-plugin-globalization.git"
