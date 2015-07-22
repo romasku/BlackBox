6157c47a8b32259b34dfb2c668c0ea2ea46d0d44
Liza <lizapopova@yandex.ua>
2015-07-22 12:48:08 +0300

    Levels are shown in blocks, level-list is NOT READY, no updating; new level

--- www/js/controllers/LevelListCtrl.js
+++ www/js/controllers/LevelListCtrl.js
@@ -1,6 +1,37 @@
-angular.module('starter.controllers.LevelListCtrl', []).controller('LevelListCtrl', function ($scope) {
-    $scope.levelCount = 5;
-    $scope.levels = [];
-    for (var i = 1; i <= $scope.levelCount; i++)
-        $scope.levels.push(i);
+angular.module('starter.controllers.LevelListCtrl', [])
+    .factory('$localstorage', ['$window', function($window) {
+        return {
+            set: function(key, value) {
+                $window.localStorage[key] = value;
+            },
+            get: function(key, defaultValue) {
+                return $window.localStorage[key] || defaultValue;
+            },
+            setObject: function(key, value) {
+                $window.localStorage[key] = JSON.stringify(value);
+            },
+            getObject: function(key) {
+                return JSON.parse($window.localStorage[key] || '{}');
+            }
+        }
+    }])
+
+    .controller('LevelListCtrl', function ($scope, $localstorage) {
+        $scope.levelCount = 6;
+        $scope.levels = [];
+        $scope.stats = [];
+        for (var i = 1; i <= $scope.levelCount; i+=3) {
+             for (var j = i; j <= i+2; j++) {
+                 if (isNaN($localstorage.getObject(j).level)) {
+                     $localstorage.setObject(j, {
+                         level: j,
+                         isCompleted: false,
+                         moves: 0,
+                         stars: 0
+                     });
+                 }
+             }
+             $scope.stats.push({col1: $localstorage.getObject(i), col2: $localstorage.getObject(i+1), col3: $localstorage.getObject(i+2)});
+             $scope.levels.push({col1: i, col2: i + 1, col3: i + 2});
+        }
 });
--- www/js/levels/5.js
+++ www/js/levels/5.js
@@ -1,9 +1,3 @@
-function calc(a) {
-    a = a.toString();
-    var ans = '';
-    for (var i = a.length - 1; i >= 0; i--) {
-        ans += a[i];
-    }
-    if (ans >= a) return parseInt(ans-a);
-    else return parseInt(a-ans);
+function calc(a){
+    return a % ((RAND % 49)+1);
 }
