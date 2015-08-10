e4b6db422aeac00a3245eb984c49929bd60ae9c7
Liza <lizapopova@yandex.ua>
2015-08-10 16:52:22 +0300

    Hints menu upgraded

--- www/js/factories/Help.js
+++ www/js/factories/Help.js
@@ -7,14 +7,14 @@ angular.module('starter.factories.Help', [])
 
             factory.template = '<p>{{\'Help_text\' | translate}}</p>' 
                     + '<div class="button-bar">'
-                    + '<button class="button button-positive button-outline" ng-click="getSmallHint();">{{\'Get_small_hint\' | translate}}</button>'
-                    + '<button class="button button-positive button-outline" ng-click="getBigHint();">{{\'Get_big_hint\' | translate}}</button>'
+                    + '<button class="button button-positive button-outline" ng-click="getSmallHint();">{{\'Get_small_hint\' | translate}}<i ng-if="levelData.smallHint" class="icon positive ion-checkmark-circled padding-left"></i></button>'
+                    + '<button class="button button-positive button-outline" ng-click="getBigHint();">{{\'Get_big_hint\' | translate}}<i ng-if="levelData.bigHint" class="icon positive ion-checkmark-circled padding-left"></i></button>'
                     + '</div>'
-                    + '<button class="button button-positive button-full" ng-click="getSolution();">{{\'Get_answer\' | translate}}</button>'
-                    + '<p class="text-center">{{\'Points\'|translate}}: {{data.points+data.extra}}</p>'
+                    + '<button class="button button-positive button-full" ng-click="getSolution();">{{\'Get_answer\' | translate}}<i ng-if="levelData.solution" class="icon ion-checkmark-circled padding-left"></i></button>'
+                    + '<p class="text-center">{{\'Points\'|translate}}: {{data.points}}</p>'
                     + '{{\'Not_enough\' | translate}}<br><br>'
                     + '<div class="button-bar">'
-                    + '<button class="button button-positive button-outline" ng-click="addExtra();">{{\'Buy\' | translate}}</button>'
+                    + '<button class="button button-positive button-outline" ng-click="change(100);">{{\'Buy\' | translate}}</button>'
                     + '<button class="button button-positive button-outline">{{\'Ad\' | translate}}</button>'
                     + '</div>';
 
@@ -41,48 +41,80 @@ angular.module('starter.factories.Help', [])
                 $scope = factory.scope;
                 $scope.data = {};
                 $scope.data.points = parseInt($window.localStorage["points"]);
-                $scope.data.extra = 0;
 
-                $scope.addExtra = function () {
-                    $scope.data.extra += 100;
+                $scope.lvl = levelsData[level].level;
+                $scope.levelData = JSON.parse($window.localStorage[$scope.lvl]);
+
+                $scope.change = function (val) {
+                    var points = JSON.parse($window.localStorage["points"] || 0);
+                    $window.localStorage["points"] = JSON.stringify(eval(points)+eval(val));
+                    $scope.data.points = parseInt($window.localStorage["points"]);
                 }
 
                 $scope.getSmallHint = function () {
-                    $scope.level = level;
-                   if ($scope.data.points + $scope.data.extra < 50) {
+                    if ($scope.levelData.smallHint) {
+                        var hint = '';
+                        if (levelsData[level - 1].smallHintType == 'template') hint = translate(levelsData[level - 1].smallHint);
+                        else {
+                            if (language == 'en') hint = levelsData[level - 1].enSmallHint;
+                            else if (language == 'ru') hint = levelsData[level - 1].ruSmallHint;
+                            else hint = levelsData[level - 1].uaSmallHint;
+                        }
+                        $ionicPopup.alert({
+                            title: translate('Get_small_hint') + ' ' + translate('Hint'),
+                            template: '<p class="text-center">' + hint + '</p>'
+                        });
+                    }
+                    else if ($scope.data.points < 50) {
                         $ionicPopup.alert({
                             title: translate('Oops'),
                             template: translate('No_points') + '<br><br>'
-                            + '<b>' + translate('Points') + '</b>: ' + ($scope.data.points + $scope.data.extra) + '<br>'
+                            + '<b>' + translate('Points') + '</b>: ' + $scope.data.points + '<br>'
                             + '<b>' + translate('Price') + '</b>: 50'
                         });
                     } else {
-                        $scope.data.points -= 50;
-                       var hint = '';
-                       if (levelsData[level-1].smallHintType == 'template') hint = translate(levelsData[level-1].smallHint);
-                       else {
-                           if (language == 'en') hint = levelsData[level-1].enSmallHint;
-                           else if (language == 'ru') hint = levelsData[level-1].ruSmallHint;
-                           else hint = levelsData[level-1].uaSmallHint;
-                       }
-                       $ionicPopup.alert({
-                           title: translate('Get_small_hint') + ' ' + translate('Hint'),
-                           template: '<p class="text-center">' + hint + '</p>'
+                        $scope.change(-50);
+                        $scope.levelData.smallHint = true;
+                        $window.localStorage[$scope.lvl] = JSON.stringify($scope.levelData);
+                        var hint = '';
+                        if (levelsData[level - 1].smallHintType == 'template') hint = translate(levelsData[level - 1].smallHint);
+                        else {
+                            if (language == 'en') hint = levelsData[level - 1].enSmallHint;
+                            else if (language == 'ru') hint = levelsData[level - 1].ruSmallHint;
+                            else hint = levelsData[level - 1].uaSmallHint;
+                        }
+                        $ionicPopup.alert({
+                            title: translate('Get_small_hint') + ' ' + translate('Hint'),
+                            template: '<p class="text-center">' + hint + '</p>'
                         });
                     }
                 }
 
                 $scope.getBigHint = function () {
-                    $scope.level = level;
-                    if ($scope.data.points + $scope.data.extra < 75) {
+                    if ($scope.levelData.bigHint) {
+                        var hint = '';
+                        if (levelsData[level-1].bigHintType == 'template') hint = translate(levelsData[level-1].bigHint);
+                        else {
+                            if (language == 'en') hint = levelsData[level-1].enBigHint;
+                            else if (language == 'ru') hint = levelsData[level-1].ruBigHint;
+                            else hint = levelsData[level-1].uaBigHint;
+                        }
+                        $ionicPopup.alert({
+                            title: translate('Get_big_hint') + ' ' + translate('Hint'),
+                            template: '<p class="text-center">' + hint + '</p>'
+                        });
+                    }
+                    else if ($scope.data.points < 75) {
                         $ionicPopup.alert({
                             title: translate('Oops'),
                             template: translate('No_points') + '<br><br>'
-                            + '<b>' + translate('Points') + '</b>: ' + ($scope.data.points + $scope.data.extra) + '<br>'
+                            + '<b>' + translate('Points') + '</b>: ' + $scope.data.points + '<br>'
                             + '<b>' + translate('Price') + '</b>: 75'
                         });
                     } else {
-                        $scope.data.points -= 75;
+                        $scope.change(-75);
+                        $scope.levelData.bigHint = true;
+                        $window.localStorage[$scope.lvl] = JSON.stringify($scope.levelData);
                         var hint = '';
                         if (levelsData[level-1].bigHintType == 'template') hint = translate(levelsData[level-1].bigHint);
                         else {
@@ -98,16 +130,27 @@ angular.module('starter.factories.Help', [])
                 }
 
                 $scope.getSolution = function () {
-                    $scope.level = level;
-                    if ($scope.data.points + $scope.data.extra < 75) {
+                    if ($scope.levelData.solution) {
+                        var hint = '';
+                        if (language == 'en') hint = levelsData[level-1].enSolution;
+                        else if (language == 'ru') hint = levelsData[level-1].ruSolution;
+                        else hint = levelsData[level-1].uaSolution;
+                        $ionicPopup.alert({
+                            title: translate('Solution'),
+                            template: '<p class="text-center">' + hint + '</p>'
+                        });
+                    }
+                    else if ($scope.data.points < 100) {
                         $ionicPopup.alert({
                             title: translate('Oops'),
                             template: translate('No_points') + '<br><br>'
-                            + '<b>' + translate('Points') + '</b>: ' + ($scope.data.points + $scope.data.extra) + '<br>'
+                            + '<b>' + translate('Points') + '</b>: ' + $scope.data.points + '<br>'
                             + '<b>' + translate('Price') + '</b>: 100'
                         });
                     } else {
-                        $scope.data.points -= 100;
+                        $scope.change(-100);
+                        $scope.levelData.solution = true;
+                        $window.localStorage[$scope.lvl] = JSON.stringify($scope.levelData);
                         var hint = '';
                         if (language == 'en') hint = levelsData[level-1].enSolution;
                         else if (language == 'ru') hint = levelsData[level-1].ruSolution;
@@ -135,8 +178,6 @@ angular.module('starter.factories.Help', [])
                         }
                     ]
                 }).then(function (res) {
-                        var points = JSON.parse($window.localStorage["points"] || 0);
-                        $window.localStorage["points"] = JSON.stringify(eval(points)+eval($scope.data.extra));
                         $scope.Keyboard.show($scope);
                         $scope.Keyboard.load(keyboardSave);
                 });
